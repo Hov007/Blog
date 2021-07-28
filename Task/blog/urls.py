3e517b1249992b7b6a752f9b696a2d949c25e7ec
Hov007 <hovayvazyan7@gmail.com>
2021-07-29 01:01:58 +0400

    starting dev

--- Task/blog/urls.py
+++ Task/blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 # We are creating separate urls.py to give routs to our particular app
 urlpatterns = [
     path("", views.index, name="index"),
-    path("hov/", views.hov, name="hov"),
+    path("newyear/", views.newyear, name="newyear"),
+    path("add/", views.add, name="add"),
     path('<str:name>/', views.greet, name="greet")
 ]
--- Task/blog/views.py
+++ Task/blog/views.py
@@ -1,12 +1,25 @@
+import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
+tasks = ["foo", "bar", "baz"]
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World!")
+    return render(request, "index.html", {
+        "tasks": tasks
+    })
 
-def hov(request):
-    return HttpResponse("Hello Hov!")
+def add(request):
+    return render(request, "add.html")
+
+def newyear(request):
+    now = datetime.datetime.now()
+    return render(request, "newyear.html", {
+        "newyear": now.month == 1 and now.day == 1
+    })
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "greet.html", {
+        "name": name.capitalize()
+    })
