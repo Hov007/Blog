7ea7e5ec9c5cda85f93303fcccd23b09cf8b18e7
Hov007 <hovayvazyan7@gmail.com>
2021-08-04 20:11:36 +0400

    Creating registration, login, logout

--- Task/blog/urls.py
+++ Task/blog/urls.py
@@ -4,7 +4,8 @@ from . import views
 # We are creating separate urls.py to give routs to our particular app
 urlpatterns = [
     path("", views.index, name="index"),
-    path("newyear/", views.newyear, name="newyear"),
-    path("add/", views.add, name="add"),
-    path('<str:name>/', views.greet, name="greet")
+    path("register/", views.register, name="register"),
+    path("login/", views.loginPage, name="login"),
+    path("logout/", views.logoutUser, name="logout"),
+    path("home/", views.home, name="home")
 ]
--- Task/blog/views.py
+++ Task/blog/views.py
@@ -1,25 +1,62 @@
-import datetime
-from django.http import HttpResponse
-from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
-tasks = ["foo", "bar", "baz"]
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+from django.contrib import messages
+
+from django.contrib.auth.decorators import login_required
+
+#Create your views here
+from .models import *
+from .forms import CreateUserForm
+
+
 def index(request):
-    return render(request, "index.html", {
-        "tasks": tasks
-    })
+    return render(request, "index.html")
 
-def add(request):
-    return render(request, "add.html")
+def register(request):
+    form = CreateUserForm() # Now form is equal to the form we created in forms.py
 
-def newyear(request):
-    now = datetime.datetime.now()
-    return render(request, "newyear.html", {
-        "newyear": now.month == 1 and now.day == 1
-    })
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid(): # checking is form is valid
+            form.save()
+            messages.success(request, "Account was created!")
 
-def greet(request, name):
-    return render(request, "greet.html", {
-        "name": name.capitalize()
-    })
+            return redirect('login')
+
+
+    context = {"form":form}
+    return render(request, "register.html", context)
+
+def loginPage(request):
+    if request.method == "POST":
+        # Taking username and password from user (from login.html)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Checking if the username and password are correct
+        user = authenticate(request, username=username, password=password)
+        # If there is a user loging in if not poping up an error message
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, "Username or Password is incorrect!")
+
+    context = {}
+    return render(request, "login.html")
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+# Decorator that bans home page without loging in
+@login_required(login_url='login')
+def home(request):
+    return render(request, "home.html")
